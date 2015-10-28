f179cbc20a79437195dcf0b3f49900bf1df8d5b3
Vladimir Kochnev <hashtable@yandex.ru>
2015-10-28 16:50:59 +0300

    Workaround for celluloid 0.17

--- lib/sidekiq/grouping/supervisor.rb
+++ lib/sidekiq/grouping/supervisor.rb
@@ -4,9 +4,16 @@ module Sidekiq
       class << self
         include Sidekiq::Grouping::Logging
 
+        if Celluloid::VERSION >= '0.17'
+        def run!
+          info 'Sidekiq::Grouping starts supervision'
+          Sidekiq::Grouping::Actor.supervise as: :sidekiq_grouping
+        end
+        else
         def run!
           info 'Sidekiq::Grouping starts supervision'
           Sidekiq::Grouping::Actor.supervise_as(:sidekiq_grouping)
+        end
         end
       end
     end
