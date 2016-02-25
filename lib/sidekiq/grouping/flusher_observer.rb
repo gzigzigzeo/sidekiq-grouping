a7bf7600f50a9c6a822f217e88492baac92bd95a
Victor Sokolov <gzigzigzeo@evilmartians.com>
2016-02-25 21:59:45 +0400

    Fixed flusher observer

--- lib/sidekiq/grouping/flusher_observer.rb
+++ lib/sidekiq/grouping/flusher_observer.rb
@@ -1,10 +1,10 @@
 class Sidekiq::Grouping::FlusherObserver
-  def update(time, result, ex)
+  def update(time, _result, ex)
     if ex.is_a?(Concurrent::TimeoutError)
       Sidekiq::Grouping.logger.error(
         "[Sidekiq::Grouping] (#{time}) Execution timed out\n"
       )
-    else
+    elsif ex.present?
       Sidekiq::Grouping.logger.error(
         "[Sidekiq::Grouping] Execution failed with error #{ex}\n"
       )
