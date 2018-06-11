7488af8702c77b4d40f2c1e4f222b1b76d68b55c
Alexey Vasiliev <leopard.not.a@gmail.com>
2018-06-11 15:18:52 +0300

    DRY code + fix rubocop issue

--- lib/sidekiq/grouping/flusher.rb
+++ lib/sidekiq/grouping/flusher.rb
@@ -3,22 +3,27 @@ class Sidekiq::Grouping::Flusher
     batches = Sidekiq::Grouping::Batch.all.map do |batch|
       batch if batch.could_flush?
     end
-    batches.compact!
-    flush_concrete(batches)
+    flush_batches(batches)
   end
 
   def force_flush_for_test!
     if defined?(::Rails) && Rails.respond_to?(:env) && !Rails.env.test?
       puts("**************************************************")
-      puts("⛔️ WARNING: force_flush_for_test! for testing API, but this is not the test environment.")
+      puts([
+        "⛔️ WARNING: force_flush_for_test! for testing API, ",
+        "but this is not the test environment."
+      ].join)
       puts("**************************************************")
     end
-    batches = Sidekiq::Grouping::Batch.all
-    batches.compact!
-    flush_concrete(batches)
+    flush_batches(Sidekiq::Grouping::Batch.all)
   end
 
   private
+
+  def flush_batches(batches)
+    batches.compact!
+    flush_concrete(batches)
+  end
 
   def flush_concrete(batches)
     return if batches.empty?
